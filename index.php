ccfd5699225c17df5d42b887a1d0c25f57e0e19a
DESKTOP-1NV9G8A\Utilisateur <mentrel.remi@gmail.com>
2020-09-19 00:36:04 +0200

    [BO][FO] cleanup file structure + switch html files to php + setup sass files wip + project miniatures resp ok

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+include '_partials/head.php';
+
+?>
+
+
+<div class="rm-c-HomeText">
+    <img src="assets/images/Home-text.svg" alt="hometext">
+</div>
+
+
+<section id="projects" class="rm-c-Content rm-c-Projects">
+  <h1 class="rm-c-Projects-title">MES RÉALISATIONS</h1>
+
+  <div class="rm-c-Projects-elements">
+    <article class="rm-c-ProjectMiniature">
+      <a href="pages/gh_page.php"  class="rm-c-ProjectMiniature-wrapper">
+        <div class="rm-c-ProjectMiniature-img">
+          <img src="assets/images/GH-miniature.jpg" alt="Miniature GH">
+        </div>
+
+        <p class="rm-c-ProjectMiniature-desc">
+          <strong class="rm-c-ProjectMiniature-desc-name">Glowy Honey Project</strong>
+        </p>
+      </a>
+    </article>
+
+    <article class="rm-c-ProjectMiniature">
+      <a href="pages/cc_page.php"  class="rm-c-ProjectMiniature-wrapper">
+        <div class="rm-c-ProjectMiniature-img">
+          <img src="assets/images/amour Lumineux.jpg" alt="Miniature CC">
+        </div>
+
+        <p class="rm-c-ProjectMiniature-desc">
+          <strong class="rm-c-ProjectMiniature-desc-name">Carnet à croquis</strong>
+        </p>  
+      </a>
+    </article>
+
+    <article class="rm-c-ProjectMiniature">
+      <a href="pages/gm_page.php"  class="rm-c-ProjectMiniature-wrapper">
+          <div class="rm-c-ProjectMiniature-img">
+            <img src="assets/images/logo-b-carre.svg" alt="Miniature GM">
+          </div>
+          
+          <p class="rm-c-ProjectMiniature-desc">
+            <strong class="rm-c-ProjectMiniature-desc-name">Le Graphisme et Moi</strong>
+          </p>
+      </a>
+    </article>
+  </div>
+</section>
+
+
+<?php include '_partials/footer.php'; ?>
---
+++ pages/gm_page.php
@@ -0,0 +1,55 @@
+<?php
+
+$page_title = 'Le graphisme & moi';
+include '../_partials/head.php';
+
+?>
+
+
+<div class="rm-c-Content rm-c-CC">
+    <div class="rm-c-GH-elements">
+        <div class="rm-c-WhiteBackground-1">
+            <div class="rm-c-Intro-CC rm-u-wrapper-GM">
+                <h1 class="rm-c-title-CC-page"><?php echo $page_title ?></h1>
+                <div class="rm-u-wrapper-text">
+                    <p class="rm-c-text">Ayant pour valeur de toujours explorer de nouveaux horizons dans l'art, j'ai appris à utiliser différents logiciels tels que Photoshop et Illustrator pour le dessin et la photo, mais également Adobe XD pour le design web.</p>
+                    <p class="rm-c-text">J'ai eu la chance pouvoir participé à de nombreux projets associatifs, tels que l'élaboration d'un logo pour un club de sport ou encore la création d'un insigne de régiment de l'armée de terre. Ce sont pour moi des occasions où je peux mettre en œuvre mes compétences et apprendre de nouvelles choses.</p>
+                </div>
+            </div>
+        </div>
+        <section class="rm-c-Logo_Section">
+            <div class="rm-u-wrapper">
+                <div class="rm-c-header-Logo">
+                    <div class="rm-c-TextStick"></div>
+                    <h3 class="rm-c-title-Text-Mark">Mon logo</h3>
+                </div>
+                <div class="rm-c-Article">
+                    <img class="rm-c-img-GM" src="../assets/images/Logo-PORTFOLIO.png" alt="icon-gh">
+                    <div class="rm-c-barre-GM-text"></div>
+                    <div class="rm-c-ContainerText">
+                        <p class="rm-c-Text-Mark-GM">Pour la conception de mon portfolio, j'ai souhaité me lancer un défi, celui de créer mon propre logo. Partant de quelques croquis, l'idée m'est venu de mettre en valeur l'identité de mon portfolio. Ce logo forme une illusion, deux lettres pour le prix d'une !</p>
+                    </div>
+                </div>
+                <div class="rm-c-Article">
+                    <div class="rm-c-ContainerText">
+                        <p class="rm-c-Text-Mark-GM">Il se compose de mes initiales, le R pour Rémi et le M pour Mentrel. Le remplissage des formes est fait de telle façon que cela donne un côté raffiné. La modernité du logo est marquée par son design minimaliste et unie au niveau de sa couleur.</p>
+                    </div>
+                    <a id="img_croquis" href="#popin_croquis" class="rm-c-img-GM-2"><img src="../assets/images/croquis_logo.jpg" alt="icon-gh"></a>
+                    <a id="popin_croquis" href="#img_croquis" class="rm-c-popin"><img src="../assets/images/croquis_logo.jpg" alt="icon-gh"></a>
+                </div>
+            </div>
+        </section>
+        <section class="rm-c-motion_Section">
+            <div class="rm-c-elements_motion">
+                <a  class="rm-c-img-motion" href="https://xd.adobe.com/view/d54da9aa-bd5c-40dc-52d6-929451ee2dbd-a37c/"><img src="../assets/images/motion_design.jpg" alt="motion_design"></a>
+                <div class="rm-c-text_motion">
+                    <h3 class="rm-c-title-CC-page">Le Motion Design</h3>
+                    <p class="rm-c-Text-Mark-GM-2">Je me suis récemment exercé dans le domaine du motion design en m'amusant à élaborer un petite animation. Cela part de ma volonté à vouloir découvrir de nouvelle technique dans le design. J'ai donc dessiné sur tablette graphique tous les éléments qui crée mon motion design, à présent, je vous laisse découvrir par vos propres yeux en cliquant sur la miniature.</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+
+
+<?php include '../_partials/footer.php'; ?>
