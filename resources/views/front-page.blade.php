9f8f02dccc93f0b0a561bbebb0210e447d1aa497
Antoine Cabrol <contact@antoinecabrol.dev>
2021-06-15 13:08:27 +0200

    [FO] Project data ok

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+    @php
+        $about = get_fields();
+    @endphp
+
     <div class="rm-c-About">
 
         <div class="rm-c-AboutIntroduction rm-u-hspace">
             <div class="rm-c-AboutIntroduction-wrapper rm-u-wrapper">
                 <div class="rm-c-AboutIntroduction-logo">
-                    <img src="{{ $about_data['introduction']['content']['logo']['url'] }}" alt="{{ $about_data['introduction']['content']['logo']['alt'] }}" />
+                    <img src="{{ $about['introduction']['content']['logo']['url'] }}" alt="{{ $about['introduction']['content']['logo']['alt'] }}" />
                 </div>
 
                 <p class="rm-c-AboutIntroduction-text">
-                    {{ $about_data['introduction']['content']['text'] }}
+                    {{ $about['introduction']['content']['text'] }}
                 </p>
 
-                <img class="rm-c-AboutIntroduction-portrait" src="{{ $about_data['introduction']['image']['url'] }}" alt="{{ $about_data['introduction']['image']['alt'] }}" />
+                <img class="rm-c-AboutIntroduction-portrait" src="{{ $about['introduction']['image']['url'] }}" alt="{{ $about['introduction']['image']['alt'] }}" />
             </div>
         </div>
 
--- resources/views/single-project.blade.php
+++ resources/views/single-project.blade.php
@@ -3,15 +3,47 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
 
+    @php
+      $project = get_fields();
+    @endphp
+
     <article @php post_class('rm-c-Project') @endphp>
       <header class="rm-c-Project-header rm-u-hspace">
           <h1>{!! get_the_title() !!}</h1>
-          <img src="" alt="" />
+          <img src="{{ $project['logo']['url'] }}" alt="{{ $project['logo']['alt'] }}" />
       </header>
 
       <div class="rm-c-Project-content rm-u-hspace">
         <div class="rm-c-Project-wrapper rm-u-wrapper">
-          TODO: ACF
+
+          <section class="rm-c-ProjectIntroduction">
+            <div class="rm-c-ProjectIntroduction-content">
+              <h2>{{ $project['introduction']['content']['title'] }}</h2>
+              {!! $project['introduction']['content']['text'] !!}
+            </div>
+
+            @if(!empty($project['introduction']['image']))
+              <img class="rm-c-ProjectIntroduction-image" src="{{ $project['introduction']['image']['url'] }}" alt="{{ $project['introduction']['image']['alt'] }}" />
+            @endif
+          </section>
+
+          @if(!empty($project['detail']))
+            <div class="rm-c-ProjectDetail">
+              <div class="rm-c-ProjectDetail-list">
+                @foreach ($project['detail'] as $section)
+                  @if(!empty($section['image']))
+                    <img class="rm-c-ProjectIntroduction-image" src="{{ $section['image']['url'] }}" alt="{{ $section['image']['alt'] }}" />
+                  @endif
+
+                  <div class="rm-c-ProjectIntroduction-content">
+                    <h2>{{ $section['content']['title'] }}</h2>
+                    {!! $section['content']['text'] !!}
+                  </div>
+                @endforeach
+              </div>
+            </div>
+          @endempty
+        
         </div>
       </div>
 
