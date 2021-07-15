e13d0d87aa77944ea395d09c1307a9e220342ab3
Antoine Cabrol <contact@antoinecabrol.dev>
2021-07-15 21:56:39 +0200

    [FO] add gtag

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,18 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  @if (!empty(App::gtag()))
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-{{ App::gtag() }}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+
+      gtag('config', "G-{{ App::gtag() }}");
+    </script>
+  @endempty
+
   @php wp_head() @endphp
 </head>
