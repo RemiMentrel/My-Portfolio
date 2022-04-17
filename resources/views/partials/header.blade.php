b8eedbf65edd5f1793f5ce14f3951eb342d1e185
Antoine Cabrol <contact@antoinecabrol.dev>
2022-04-17 08:59:47 +0200

    [FO] Burger menu ok

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,15 @@
 <header class="rm-c-Header rm-u-hspace">
   <div class="rm-c-Header-wrapper rm-u-wrapper" data-size="large">
-    <nav class="rm-c-Header-navigation">
+    <nav class="rm-c-Header-navigation" data-is="showHide">
       <a class="rm-c-Header-navigation-logo" href="{{ home_url('/') }}">
         <img src="@asset('images/logo/logo_mini_light.svg')" alt="{{ get_bloginfo('name', 'display') }}" />
       </a>
 
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'rm-c-Navigation']) !!}
+        <span class="rm-c-Header-navigation-toggler" aria-hidden="true" data-show-hide="toggler"></span>
+        <div class="rm-c-Header-navigation-wrapper" data-show-hide="content">
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'rm-c-Navigation']) !!}
+        </div>
       @endif
     </nav>
   </div>
